93d6762ffe03ae2f6fcaff0a620b864cf74b0f13
mAAdhaTTah <jamesorodig@gmail.com>
2019-05-06 19:25:32 -0400

    Switch to readDay to group by day instead of week
    A lot of articles are read in a given week, so this will make the display
    a lot easier.

--- src/components/Day.js
+++ src/components/Day.js
@@ -7,7 +7,7 @@ const ReadLink = ({ url, title, excerpt, readAt }) => (
       href={url}
       className="no-underline block text-secondary-color bg-accent-dark p-3 rounded mb-2"
     >
-      {title}
+      {title || url}
     </a>
     <div className="bg-primary-color p-3 rounded mb-2">
       <p className="max-h-4 overflow-hidden">{excerpt}</p>
@@ -26,9 +26,9 @@ ReadLink.propTypes = {
   readAt: PropTypes.string.isRequired,
 };
 
-const Week = ({ weekOf, links }) => (
+const Day = ({ dayOf, links }) => (
   <div className="max-w-md mx-auto">
-    <h2 className="font-header py-3 text-2xl px-3">Week of {weekOf}</h2>
+    <h2 className="font-header py-3 text-2xl px-3">Day of {dayOf}</h2>
     <ul className="font-body list-reset">
       {links.map(link => (
         <ReadLink key={link.id} {...link} />
@@ -37,9 +37,9 @@ const Week = ({ weekOf, links }) => (
   </div>
 );
 
-Week.propTypes = {
-  weekOf: PropTypes.string.isRequired,
+Day.propTypes = {
+  dayOf: PropTypes.string.isRequired,
   links: PropTypes.array.isRequired,
 };
 
-export default Week;
+export default Day;
