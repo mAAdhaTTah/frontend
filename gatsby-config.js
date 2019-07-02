71679f1b47dba974a756b90591d5bc39609e1b1e
mAAdhaTTah <jamesorodig@gmail.com>
2019-07-01 23:30:19 -0400

    Add JSON transformer to use JSON files
    We're going to keep the resume data in JSON.

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    },
+    'gatsby-transformer-json',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
