66a3e3c0c6145b6923d8e43cf082e836eb1e0feb
mAAdhaTTah <jamesorodig@gmail.com>
2019-07-19 23:16:09 -0400

    Add simple pagination

--- src/templates/Post/Archive.js
+++ src/templates/Post/Archive.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import cc from 'classcat';
+import { FaLongArrowAltRight, FaLongArrowAltLeft } from 'react-icons/fa';
 import { Layout, Post } from '../../components';
+
+const paginationClass = cc([
+  'clearfix',
+  'max-w-xl',
+  'mx-auto',
+  'items-stretch',
+]);
+
+const pageClass = cc([
+  'bg-darkg',
+  'p-5',
+  'rounded',
+  'text-primary',
+  'font-body',
+  'no-underline',
+  'flex',
+  'align-center'
+]);
 
 const PostArchive = ({ pageContext }) => {
   return (
@@ -8,6 +29,28 @@ const PostArchive = ({ pageContext }) => {
         {pageContext.posts.map(({ node }) => (
           <Post.Excerpt key={node.id} {...node} />
         ))}
+      </div>
+      <div className={paginationClass}>
+        {pageContext.pageNumber > 1 && (
+          <Link
+            to={`/writing/${
+              pageContext.pageNumber !== 2
+                ? `page/${pageContext.pageNumber - 1}`
+                : ''
+            }`}
+            className={cc([pageClass, 'float-left'])}
+          >
+            <FaLongArrowAltLeft className="mr-3" /> Previous
+          </Link>
+        )}
+        {pageContext.hasNextPage && (
+          <Link
+            to={`/writing/page/${pageContext.pageNumber + 1}/`}
+            className={cc([pageClass, 'float-right'])}
+          >
+            Next <FaLongArrowAltRight className="ml-3"  />
+          </Link>
+        )}
       </div>
     </Layout>
   );
