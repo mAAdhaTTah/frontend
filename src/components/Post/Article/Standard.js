8ae60564e81686cbd46d6098dd9ee16a755d7b56
mAAdhaTTah <jamesorodig@gmail.com>
2020-08-29 17:22:32 -0400

    Fix header design when media image is missing

--- src/components/Post/Article/Standard.js
+++ src/components/Post/Article/Standard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cc from 'classcat';
 import { useParseHTML } from '../../../hooks';
 import { ArticleHeader } from '../../typography';
 import EntryMeta from '../../EntryMeta';
@@ -16,9 +17,17 @@ const StandardFormat = ({
 }) => {
   return (
     <Article>
-      <header className="relative">
+      <header className="relative mb-5">
         {media && <MediaImage media={media} />}
-        <div className="absolute pin-x-center pin-y-bottom w-full bg-etched rounded p-5">
+        <div
+          className={cc([
+            { absolute: media, 'pin-x-center': media, 'pin-y-bottom': media },
+            'w-full',
+            'bg-etched',
+            'rounded',
+            'p-5',
+          ])}
+        >
           <ArticleHeader className="text-black text-center mb-5">
             {useParseHTML(title)}
           </ArticleHeader>
