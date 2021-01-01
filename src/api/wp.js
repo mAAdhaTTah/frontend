003e0d7bbf976e4b9c14a97cb8588720c95bf44f
James DiGioia <jamesorodig@gmail.com>
2021-01-01 11:59:07 -0500

    Rewrite the BE domain to the FE domain in client (#173)
    We should really fix this in WP if possible but this is a reasonable
    workaround for now. Bump Node version for `replaceAll`.

--- src/api/wp.js
+++ src/api/wp.js
@@ -1,6 +1,33 @@
 import axios from 'axios';
 import { shared, server } from '../config';
 
+const deepReplaceDomain = value => {
+  // Ignore empty strings, null, undefined, functions, & numbers.
+  if (!value || typeof value === 'number' || typeof value === 'function') {
+    return value;
+  }
+
+  if (Array.isArray(value)) {
+    return value.map(deepReplaceDomain);
+  }
+
+  if (typeof value === 'object') {
+    for (const key in value) {
+      value[key] = deepReplaceDomain(value[key]);
+    }
+  }
+
+  // Rewrite strings that don't point to the API.
+  if (typeof value === 'string' && !value.includes('wp-json')) {
+    return value.replaceAll(
+      shared.WP_API_DOMAIN,
+      shared.WP_API_DOMAIN.replace('backend.', ''),
+    );
+  }
+
+  return value;
+};
+
 export const wp = axios.create({
   baseURL: `https://${shared.WP_API_DOMAIN}`,
 });
@@ -11,5 +38,10 @@ wp.interceptors.request.use(value => {
     'binary',
   ).toString('base64')}`;
 
+  return value;
+});
+
+wp.interceptors.response.use(value => {
+  value.data = deepReplaceDomain(value.data);
   return value;
 });
