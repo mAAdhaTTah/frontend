c2343a99f127b2134829ddf2256ad6e35924d3dc
mAAdhaTTah <jamesorodig@gmail.com>
2021-02-02 11:10:06 -0500

    Repro for eslint-plugin-import bug
    SQLite removed because it's causing build errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['brookjs', 'prettier'],
+  plugins: ['import'],
+  rules: {
+    'import/first': 'error',
+    'import/no-amd': 'error',
+    'import/no-anonymous-default-export': 'warn',
+    'import/no-webpack-loader-syntax': 'error',
+    'import/export': 'error',
+    'import/default': 'error',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-absolute-path': 'error',
+    'import/no-cycle': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-default-export': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        'newlines-between': 'never',
+      },
+    ],
+  },
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,10 @@
-import * as Post from './Post';
+export * as Post from './Post';
 
 export { default as Article } from './Article';
 export { default as Header } from './Header';
 export { default as Category } from './Category';
 export { Layout } from './Layout';
 export { default as Nav } from './Nav';
-export { Post };
 export { default as Tag } from './Tag';
 export { default as Day } from './Day';
 export * from './typography';
