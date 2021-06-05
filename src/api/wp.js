90c6ef9003473f1d57d4d249eecec057da66a60f
mAAdhaTTah <jamesorodig@gmail.com>
2021-06-05 18:17:51 -0400

    Lower node version to 14
    This is the latest on Vercel.

--- src/api/wp.js
+++ src/api/wp.js
@@ -1,6 +1,27 @@
 import axios from 'axios';
 import { shared, server } from '../config';
 
+/**
+ * String.prototype.replaceAll() polyfill
+ * https://gomakethings.com/how-to-replace-a-section-of-a-string-with-another-one-with-vanilla-js/
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
+if (!String.prototype.replaceAll) {
+  // eslint-disable-next-line
+  String.prototype.replaceAll = function (str, newStr) {
+    // If a regex pattern
+    if (
+      Object.prototype.toString.call(str).toLowerCase() === '[object regexp]'
+    ) {
+      return this.replace(str, newStr);
+    }
+
+    // If a string
+    return this.replace(new RegExp(str, 'g'), newStr);
+  };
+}
+
 const deepReplaceDomain = value => {
   // Ignore empty strings, null, undefined, functions, & numbers.
   if (!value || typeof value === 'number' || typeof value === 'function') {
