1a9b5aec34f056db505d058723662ca84a4a4875
James DiGioia <jamesorodig@gmail.com>
2021-09-19 14:38:53 -0400

    Upgrade next (#422)
    * Bump storybook-addon-next-router from 2.0.4 to 3.0.8
    
    Bumps [storybook-addon-next-router](https://github.com/lifeiscontent/storybook-addon-next-router) from 2.0.4 to 3.0.8.
    - [Release notes](https://github.com/lifeiscontent/storybook-addon-next-router/releases)
    - [Commits](https://github.com/lifeiscontent/storybook-addon-next-router/compare/v2.0.4...v3.0.8)
    
    ---
    updated-dependencies:
    - dependency-name: storybook-addon-next-router
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump next from 10.2.3 to 11.1.2
    
    Bumps [next](https://github.com/vercel/next.js) from 10.2.3 to 11.1.2.
    - [Release notes](https://github.com/vercel/next.js/releases)
    - [Changelog](https://github.com/vercel/next.js/blob/canary/release.js)
    - [Commits](https://github.com/vercel/next.js/compare/v10.2.3...v11.1.2)
    
    ---
    updated-dependencies:
    - dependency-name: next
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    # Conflicts:
    #	package-lock.json
    
    * Fix addon integration
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-docs',
+    'storybook-addon-next-router',
     {
       name: '@storybook/addon-postcss',
       options: {
