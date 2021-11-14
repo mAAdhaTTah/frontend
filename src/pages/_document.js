c8b7255d0a3d1a86fe38ce7cf1aed8fda120ca51
mAAdhaTTah <jamesorodig@gmail.com>
2021-11-14 18:37:55 -0500

    Convert Document to FC
    We don't need `getInitialProps` there so remove it & simplify.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,31 +1,24 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class Document extends NextDocument {
-  static async getInitialProps(ctx) {
-    const initialProps = await NextDocument.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Muli&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Ovo&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Muli&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ovo&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
 export default Document;
