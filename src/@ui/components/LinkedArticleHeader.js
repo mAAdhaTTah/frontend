851c1bc45584f3d60608bf8e7d867251af76747a
mAAdhaTTah <jamesorodig@gmail.com>
2023-01-16 13:29:39 -0500

    Run codemod for links
    `next/image` doesn't need to change (still works fine).

--- src/@ui/components/LinkedArticleHeader.js
+++ src/@ui/components/LinkedArticleHeader.js
@@ -4,9 +4,7 @@ import Link from 'next/link';
 export const LinkedArticleHeader = ({ href, children }) => {
   return (
     <Heading level={1} variant="article">
-      <Link href={href}>
-        <a>{children}</a>
-      </Link>
+      <Link href={href}>{children}</Link>
     </Heading>
   );
 };
