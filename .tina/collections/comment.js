8fdf2877ff884a31bcdc543596c95d6c9d59f6cf
mAAdhaTTah <jamesorodig@gmail.com>
2023-02-01 10:19:41 -0500

    Migrate comments
    Not going to render them yet but move them out of WordPress.

--- .tina/collections/comment.js
+++ .tina/collections/comment.js
@@ -2,7 +2,27 @@ import { descriptionField, nameField } from '../fields';
 
 /**
  * @typedef {import('tinacms').Collection} TinaCollection
+ * @typedef {import('tinacms').SchemaField} Field
  */
+
+/** @type {Field} */
+const authorField = {
+  type: 'object',
+  name: 'author',
+  label: 'Author',
+  fields: [
+    {
+      name: 'name',
+      label: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'url',
+      label: 'URL',
+      type: 'string',
+    },
+  ],
+};
 
 /**
  * @type TinaCollection
@@ -11,5 +31,24 @@ export const commentCollection = {
   name: 'comment',
   label: 'Comments',
   path: 'content/comments',
-  fields: [nameField, { ...descriptionField, isBody: true }],
+  fields: [
+    authorField,
+    {
+      type: 'rich-text',
+      name: 'body',
+      label: 'Body',
+    },
+    {
+      type: 'reference',
+      name: 'post',
+      label: 'Post',
+      collections: ['post'],
+    },
+    {
+      type: 'reference',
+      name: 'parent',
+      label: 'Parent',
+      collections: ['comment'],
+    },
+  ],
 };
--- src/pages/api/migrate/wordpress.js
+++ src/pages/api/migrate/wordpress.js
@@ -1,21 +1,11 @@
-import slugify from '@sindresorhus/slugify';
 import parseLinkHeader from 'parse-link-header';
 import { wp } from '@wp/api/wp';
+import { contentToChildren } from '@wp/migrate';
 import { gql, createClient } from 'tinacms';
 
 const updateCommentMutation = gql`
   mutation updateComment($relativePath: String!, $params: CommentMutation!) {
     updateComment(relativePath: $relativePath, params: $params) {
-      ... on Document {
-        id
-      }
-    }
-  }
-`;
-
-const updatePostMutation = gql`
-  mutation updatePost($relativePath: String!, $params: PostMutation!) {
-    updatePost(relativePath: $relativePath, params: $params) {
       ... on Document {
         id
       }
@@ -39,94 +29,32 @@ async function* getAllForUrl(url, params = {}, page = 1) {
   }
 }
 
-const getSlugFromPost = post => post.slug || slugify(post.title.raw);
-
-const prettyPrint = x => JSON.stringify(x, null, '  ');
-
-const plural = taxonomy => {
-  switch (taxonomy) {
-    case 'category':
-      return 'categories';
-    case 'post_tag':
-    case 'tag':
-      return 'tags';
-    default:
-      throw new Error(`unknown taxonomy ${taxonomy}`);
-  }
-};
-
-const categoriesAndTags = async post => {
-  return post._embedded['wp:term'].reduce(
-    (results, terms) => {
-      for (const term of terms) {
-        results[plural(term.taxonomy)].push({
-          reference: `content/${plural(term.taxonomy)}/${term.slug}.md`,
-        });
-      }
-      return results;
-    },
-    {
-      categories: [],
-      tags: [],
-    },
-  );
-};
-
-const getPostParams = async post => ({
-  [post.format]: await categoriesAndTags(post),
-});
-
 /** @type {import('next').NextApiHandler} */
 const handler = async (_req, res) => {
   const tina = createClient({ isLocalClient: true });
 
-  for await (const post of getAllForUrl(`/wp-json/wp/v2/posts`, {
-    context: 'edit',
-    status: 'publish',
-    _embed: 'wp:term',
-  })) {
-    const slug = getSlugFromPost(post);
-    const params = await getPostParams(post);
-    await tina
-      .request(updatePostMutation, {
-        variables: {
-          relativePath: `${slug}.md`,
-          params,
-        },
-      })
-      .catch(err => {
-        console.log(prettyPrint(params));
-        throw err;
-      });
-  }
-
-  for await (const post of getAllForUrl(`/wp-json/wp/v2/posts`, {
-    context: 'edit',
-    status: 'draft',
-    _embed: 'wp:term',
-  })) {
-    const slug = getSlugFromPost(post);
-    const params = await getPostParams(post);
-    await tina
-      .request(updatePostMutation, {
-        variables: {
-          relativePath: `${slug}.md`,
-          params,
-        },
-      })
-      .catch(err => {
-        console.log(prettyPrint(params));
-        throw err;
-      });
-  }
-
   for await (const comment of getAllForUrl(`/wp-json/wp/v2/comments`, {
-    _embed: 'up,post',
+    context: 'edit',
+    _embed: 'up,in-reply-to',
   })) {
+    console.log(comment);
     await tina.request(updateCommentMutation, {
       variables: {
         relativePath: `${comment.id}.md`,
-        params: {},
+        params: {
+          author: {
+            name: comment.author_name,
+            url: comment.author_url,
+          },
+          body: {
+            type: 'root',
+            children: contentToChildren(comment.content.rendered),
+          },
+          post: `content/posts/${comment._embedded.up[0].slug}.md`,
+          parent: comment._embedded['in-reply-to']?.[0]
+            ? `content/comments/${comment._embedded['in-reply-to'][0].id}.md`
+            : null,
+        },
       },
     });
   }
