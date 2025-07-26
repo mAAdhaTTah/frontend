f5b50e73290b2b9673683737461e4e0dc027e62b
mAAdhaTTah <jamesorodig@gmail.com>
2025-07-26 18:41:46 -0400

    Add generic not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>
+        I recently refreshed the process by which I export content to my site.
+        If you landed here and expected to find something here, please reach out
+        to me at: jamesorodig@gmail.com.
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
