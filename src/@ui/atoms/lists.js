5f8f80fbf168a7a0bbc08c7c9f60f7916cea3fd2
mAAdhaTTah <jamesorodig@gmail.com>
2025-08-28 17:04:47 -0400

    Fix left padding on lists

--- src/@ui/atoms/lists.js
+++ src/@ui/atoms/lists.js
@@ -1,9 +1,9 @@
 export const Ul = ({ children }) => {
-  return <ul className="list-disc pl-4 mb-5">{children}</ul>;
+  return <ul className="list-disc pl-8 mb-5">{children}</ul>;
 };
 
 export const Ol = ({ children }) => {
-  return <ol className="list-decimal pl-4 mb-5">{children}</ol>;
+  return <ol className="list-decimal pl-8 mb-5">{children}</ol>;
 };
 
 export const Li = ({ id, children }) => {
