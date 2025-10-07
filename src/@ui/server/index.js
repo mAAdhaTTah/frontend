ea66bafa6ae6c7748bc70d662c23fcb537d718db
mAAdhaTTah <jamesorodig@gmail.com>
2025-10-06 23:36:42 -0400

    Re-add image and extend timeout

--- src/@ui/server/index.js
+++ src/@ui/server/index.js
@@ -24,7 +24,9 @@ const TweetPage = async ({ id }) => {
 };
 
 const getImageInfo = cache(async src => {
-  const res = await fetch(src);
+  const res = await fetch(src, {
+    signal: AbortSignal.timeout(60 * 1000), // <=== HERE
+  });
   const buffer = Buffer.from(await res.arrayBuffer());
   const {
     base64,
