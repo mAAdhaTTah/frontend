1e913308506bd9608e87fe73e637d654c7cb66ee
mAAdhaTTah <jamesorodig@gmail.com>
2025-10-06 22:51:23 -0400

    Fix postcss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  plugins: [
-    'postcss-import',
-    ['tailwindcss', require('./tailwind.config.js')],
-    'postcss-nested',
-    'postcss-for',
-    'autoprefixer',
-  ],
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 };
